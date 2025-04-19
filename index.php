046f8be97fd54011e824a8f9afed4e730aa49b98
Nagaraj B M <124360626+naagu-2@users.noreply.github.com>
2025-04-19 11:17:46 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,141 +1,188 @@
+<?php
+session_start();
+//error_reporting(0);
+include('admin/includes/dbconnection.php');
+?>
 <!doctype html>
-<html lang="en">
+<html class="no-js" lang="en">
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
-        <meta name="author" content="Coderthemes">
-
-        <!-- App Favicon -->
-        <link rel="shortcut icon" href="assets/images/favicon.ico">
-
-        <!-- App title -->
-        <title>Uplon - Bootstrap 4 Responsive Admin Dashboard Template</title>
-
-        <!-- Bootstrap CSS -->
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-
-        <!-- App CSS -->
-        <link href="assets/css/style.css" rel="stylesheet" type="text/css" />
-
-        <!-- Modernizr js -->
-        <script src="assets/js/modernizr.min.js"></script>
-
-    </head>
-
-
-    <body>
-
-        <div class="account-pages"></div>
-        <div class="clearfix"></div>
-        <div class="wrapper-page">
-
-        	<div class="account-bg">
-                <div class="card-box mb-0">
-                    <div class="text-center m-t-20">
-                        <a href="index.html" class="logo">
-                            <i class="zmdi zmdi-group-work icon-c-logo"></i>
-                            <span>Uplon</span>
-                        </a>
-                    </div>
-                    <div class="m-t-10 p-20">
-                        <div class="row">
-                            <div class="col-12 text-center">
-                                <h6 class="text-muted text-uppercase m-b-0 m-t-0">Sign In</h6>
-                            </div>
-                        </div>
-                        <form class="m-t-20" action="index.html">
-
-                            <div class="form-group row">
-                                <div class="col-12">
-                                    <input class="form-control" type="text" required="" placeholder="Username">
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <div class="col-12">
-                                    <input class="form-control" type="password" required="" placeholder="Password">
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <div class="col-12">
-                                    <div class="checkbox checkbox-custom">
-                                        <input id="checkbox-signup" type="checkbox">
-                                        <label for="checkbox-signup">
-                                            Remember me
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="form-group text-center row m-t-10">
-                                <div class="col-12">
-                                    <button class="btn btn-success btn-block waves-effect waves-light" type="submit">Log In</button>
-                                </div>
-                            </div>
-
-                            <div class="form-group row m-t-30 mb-0">
-                                <div class="col-12">
-                                    <a href="pages-recoverpw.html" class="text-muted"><i class="fa fa-lock m-r-5"></i> Forgot your password?</a>
-                                </div>
-                            </div>
-
-                            <div class="form-group row m-t-30 mb-0">
-                                <div class="col-12 text-center">
-                                    <h5 class="text-muted"><b>Sign in with</b></h5>
-                                </div>
-                            </div>
-
-                            <div class="form-group row mb-0 text-center">
-                                <div class="col-12">
-                                    <button type="button" class="btn btn-facebook waves-effect font-14 waves-light m-t-20">
-                                       <i class="fa fa-facebook m-r-5"></i> Facebook
-                                    </button>
-
-                                    <button type="button" class="btn btn-twitter waves-effect font-14 waves-light m-t-20">
-                                       <i class="fa fa-twitter m-r-5"></i> Twitter
-                                    </button>
-
-                                    <button type="button" class="btn btn-googleplus waves-effect font-14 waves-light m-t-20">
-                                       <i class="fa fa-google-plus m-r-5"></i> Google+
-                                    </button>
-                                </div>
-                            </div>
-
-                        </form>
-
-                    </div>
-
-                    <div class="clearfix"></div>
-                </div>
-            </div>
-            <!-- end card-box-->
-
-            <div class="m-t-20">
-                <div class="text-center">
-                    <p class="text-white">Don't have an account? <a href="pages-register.html" class="text-white m-l-5"><b>Sign Up</b></a></p>
-                </div>
-            </div>
-
-        </div>
-        <!-- end wrapper page -->
-
-
+<head>
+    
+    <!--====== Title ======-->
+    <title>IFSC Code Finder Portal | Home</title>
+    
+    <!--====== Slick CSS ======-->
+    <link rel="stylesheet" href="assets/css/slick.css">
         
+    <!--====== Font Awesome CSS ======-->
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+        
+    <!--====== Line Icons CSS ======-->
+    <link rel="stylesheet" href="assets/css/LineIcons.css">
+        
+    <!--====== Animate CSS ======-->
+    <link rel="stylesheet" href="assets/css/animate.css">
+        
+    <!--====== Magnific Popup CSS ======-->
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+        
+    <!--====== Bootstrap CSS ======-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    
+    <!--====== Default CSS ======-->
+    <link rel="stylesheet" href="assets/css/default.css">
+    
+    <!--====== Style CSS ======-->
+    <link rel="stylesheet" href="assets/css/style.css">
+    
+</head>
 
-        <!-- jQuery  -->
-        <script src="assets/js/jquery.min.js"></script>
-        <script src="assets/js/bootstrap.bundle.min.js"></script>
-        <script src="assets/js/detect.js"></script>
-        <script src="assets/js/waves.js"></script>
-        <script src="assets/js/jquery.nicescroll.js"></script>
-        <script src="../plugins/switchery/switchery.min.js"></script>
+<body>
+    <!--[if IE]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+   
+   
+    <!--====== PRELOADER PART START ======-->
 
-        <!-- App js -->
-        <script src="assets/js/jquery.core.js"></script>
-        <script src="assets/js/jquery.app.js"></script>
+    <div class="preloader">
+        <div class="loader">
+            <div class="ytp-spinner">
+                <div class="ytp-spinner-container">
+                    <div class="ytp-spinner-rotator">
+                        <div class="ytp-spinner-left">
+                            <div class="ytp-spinner-circle"></div>
+                        </div>
+                        <div class="ytp-spinner-right">
+                            <div class="ytp-spinner-circle"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-    </body>
+    <!--====== PRELOADER PART ENDS ======-->
+    
+    <!--====== HEADER PART START ======-->
+    
+    <header class="header-area">
+        <div class="navbar-area headroom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <nav class="navbar navbar-expand-lg">
+                           <h3 style="color: red;padding-right: 50px;">IFSC Code Finder Portal</h3>
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="toggler-icon"></span>
+                                <span class="toggler-icon"></span>
+                                <span class="toggler-icon"></span>
+                            </button>
+
+                            <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
+                                <ul id="nav" class="navbar-nav m-auto">
+                                    <li class="nav-item active">
+                                        <a href="index.php">Home</a>
+                                    </li>
+                                   
+                                    <li class="nav-item">
+                                        <a href="admin/login.php">Admin</a>
+                                    </li>
+                                   
+                        </nav> <!-- navbar -->
+                    </div>
+                </div> <!-- row -->
+            </div> <!-- container -->
+        </div> <!-- navbar area -->
+        
+        <div id="home" class="header-hero bg_cover d-lg-flex align-items-center" style="background-image: url(assets/images/header-hero.jpg)">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-7">
+                        <div class="header-hero-content">
+                            <h1 class="hero-title wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.2s"><b>Search</b> <span>Bank</span> Detail <b>by one click.</b></h1>
+                            <div class="header-singup wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.8s">
+                                <form action="search.php" method="post" name="search">
+                                <input type="text" placeholder="Enter Bank Name/Zipcode/Branch" name="searchifsccode">
+                                <button class="main-btn"  name="search" id="submit" type="submit">Search</button></form>
+                            </div>
+                        </div> <!-- header hero content -->
+                    </div>
+                </div> <!-- row -->
+            </div> <!-- container -->
+            <div class="header-hero-image d-flex align-items-center wow fadeInRightBig" data-wow-duration="1s" data-wow-delay="1.1s">
+                <div class="image">
+                    <img src="assets/images/hero-image.png" alt="Hero Image">
+                </div>
+            </div> <!-- header hero image -->
+        </div> <!-- header hero -->
+    </header>
+    
+ 
+    <!--====== FOOTER PART START ======-->
+    
+    <footer id="footer" class="footer-area bg_cover" style="background-image: url(assets/images/footer-bg.jpg)">
+        <div class="container">
+            <div class="footer-copyright text-center">
+                <p class="text">© <?php echo date('Y');?> IFSC Code finder Portal</p>
+            </div>
+        </div> <!-- container -->
+    </footer>
+    
+    <!--====== FOOTER PART ENDS ======-->
+    
+    <!--====== BACK TOP TOP PART START ======-->
+
+    <a href="#" class="back-to-top"><i class="lni-chevron-up"></i></a>
+
+    <!--====== BACK TOP TOP PART ENDS ======-->  
+
+
+
+
+    <!--====== Jquery js ======-->
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="assets/js/vendor/modernizr-3.7.1.min.js"></script>
+    
+    <!--====== Bootstrap js ======-->
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    
+    <!--====== Slick js ======-->
+    <script src="assets/js/slick.min.js"></script>
+    
+    <!--====== Isotope js ======-->
+    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+    
+    <!--====== Counter Up js ======-->
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    
+    <!--====== Circles js ======-->
+    <script src="assets/js/circles.min.js"></script>
+    
+    <!--====== Appear js ======-->
+    <script src="assets/js/jquery.appear.min.js"></script>
+    
+    <!--====== WOW js ======-->
+    <script src="assets/js/wow.min.js"></script>
+    
+    <!--====== Headroom js ======-->
+    <script src="assets/js/headroom.min.js"></script>
+    
+    <!--====== Jquery Nav js ======-->
+    <script src="assets/js/jquery.nav.js"></script>
+    
+    <!--====== Scroll It js ======-->
+    <script src="assets/js/scrollIt.min.js"></script>
+    
+    <!--====== Magnific Popup js ======-->
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    
+    <!--====== Main js ======-->
+    <script src="assets/js/main.js"></script>
+    
+</body>
+
 </html>
